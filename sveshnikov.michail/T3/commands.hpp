5db896e643136a555ad3c3e581354ab0770be1ee
svesh3000 <russkij2006@mail.ru>
2025-06-22 03:30:21 +0300

    Fix commands hpp

--- sveshnikov.michail/T3/commands.hpp
+++ sveshnikov.michail/T3/commands.hpp
@@ -5,12 +5,12 @@
 
 namespace sveshnikov
 {
-  void area(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
-  void max(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
-  void min(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
-  void count(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
-  void maxseq(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
-  void rmecho(std::istream &in, std::ostream &out, std::vector< Polygon > &shapes);
+  void area(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
+  void max(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
+  void min(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
+  void count(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
+  void maxseq(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
+  void rmecho(std::istream &in, std::ostream &out, const std::vector< Polygon > &shapes);
 }
 
 #endif
